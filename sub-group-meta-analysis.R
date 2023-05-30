f3a89b3be3a66860a2956d7cf95c09b7886139cc
Mattia A. Fritz <mattia.fritz@unige.ch>
2023-05-31 01:59:08 +0200

    Scripts

--- sub-group-meta-analysis.R
+++ sub-group-meta-analysis.R
@@ -4,14 +4,14 @@ library(meta)
 
 
 data_meta_analysis <- tibble::tribble(
-  ~author,         ~n.e,    ~mean.e, ~sd.e,     ~n.c, ~mean.c, ~sd.c,
-  "Expérience 1",    50,      115.0,  15.0,       50,   100.0,  15.0,
-  "Expérience 2",    20,      125.5,  15.9,       20,   102.8,  13.8,
-  "Expérience 3",    10,      105.0,  13.0,       10,   111.0,  16.4,
-  "Expérience 4",    90,      111.4,  13.7,       90,   101.5,  11.9,
-  "Expérience 5",    25,      101.5,  16.2,       25,   106.2,  12.8,
-  "Expérience 6",    60,      117.5,  18.2,       60,   103.4,  15.6,
-  "Expérience 7",    30,      112.1,  14.7,       30,   109.1,  16.3,
+  ~author,         ~n.e,    ~mean.e, ~sd.e,     ~n.c, ~mean.c, ~sd.c,  ~factor,
+  "Expérience 1",    50,      115.0,  15.0,       50,   100.0,  15.0,  "A",
+  "Expérience 2",    20,      125.5,  15.9,       20,   102.8,  13.8,  "B",
+  "Expérience 3",    10,      105.0,  13.0,       10,   111.0,  16.4,  "A",
+  "Expérience 4",    90,      111.4,  13.7,       90,   101.5,  11.9,  "B",
+  "Expérience 5",    25,      101.5,  16.2,       25,   106.2,  12.8,  "A",
+  "Expérience 6",    60,      117.5,  18.2,       60,   103.4,  15.6,  "B",
+  "Expérience 7",    30,      112.1,  14.7,       30,   109.1,  16.3,  "A"
 )
 
 
@@ -30,7 +30,8 @@ m.cont <- metacont(n.e = n.e,
                    method.tau = "REM",
                    hakn = TRUE,
                    prediction = TRUE,
-                   title = "Exemple de méta-analysis"
+                   title = "Exemple de méta-analysis",
+                   subgroup = factor
 )
 
 forest.meta(
